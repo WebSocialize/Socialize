bec9b3adf76ea693ac662ec56eb7aa7fc6c6d5f2
alessscop <alessandroscopigno@gmail.com>
2013-10-10 16:57:48 +0200

    controller added

---
+++ Control/CRegistrazione.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Classe registrazione
+ */
+
+class CRegistrazione {
+    
+    //attributi
+    private $email;
+    private $password;
+    
+    //metodi
+    public function CreaUtente(){
+        
+    }
+    
+    /*
+     * email attivazione
+     */
+    public function EmailAttiv() {
+        
+    }
+    
+    /*
+     * attivazione
+     */
+    public function attivazione() {
+        
+    }
+    
+}
+
+?>
