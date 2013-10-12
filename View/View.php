e44bbbf1ee0a3be6fc1b63ab27e6c281f7d8b4c1
alessscop <alessandroscopigno@gmail.com>
2013-10-12 11:25:37 +0200

    aggiunto classe View

---
+++ View/View.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * classe view inizializza smarty e il suo ambiente di lavoro
+ */
+
+require '/lib/smarty/Smarty.class.php/';
+
+class View extends Smarty {
+    public function __construct() {
+        $this->smarty();
+        $this->template_dir = 'templates/template';
+        $this->compile_dir = 'templates/templates_c';
+        $this->config_dir = 'templates/configs';
+        $this->cache_dir = 'templates/cache';
+    }
+    
+    /*
+     * funzione che assegna un template passato come parametro
+     */
+    public function setTemplate( $template ){
+        $this->display( $template );
+    }
+    
+}
+
+
+?>
