55807a582d09b1d74a358c67e07ceaa4e11eef0c
alessscop <alessandroscopigno@gmail.com>
2013-10-12 14:09:02 +0200

    aggiornato qualcosa

--- Control/CRegistrazione.php
+++ Control/CRegistrazione.php
@@ -1,8 +1,7 @@
 <?php
 
 /*
- * Classe registrazione CASA
- abitacolo
+ * Classe registrazione
  */
 
 class CRegistrazione {
@@ -13,6 +12,13 @@ class CRegistrazione {
     
     //metodi
     public function CreaUtente(){
+        /*
+         * prendo i dati tramite il metodo dati della funzione getDati di VRegistrazione
+         * controllo che email uguale non sia presente nella BD, poi controllo che password 
+         * e la sua conferma coincidano. In caso di errori li salvo in un array e poi li passero'
+         * a VRegistrazione.
+         * Prima bisogna capire come usare Singleton per fare una sola istanza etc
+         */
         
     }
     
---
+++ View/VRegistrazione.php
@@ -0,0 +1,49 @@
+<?php
+
+class VRegistrazione {
+    
+    //attributi
+    
+    //metodi
+    
+    /*
+     * metodo per prendere il nome dai dati inseriti dall'utente 
+     */
+    public function getEmail() {
+        if( isset( $_REQUEST[ 'email' ] ) ) 
+            return $_REQUEST[ 'email' ];
+        else
+            return FALSE;
+    }
+    
+    /*
+     * ritorna la password inserita dall'utente
+     */
+    public function getPassword() {
+        if( isset( $_REQUEST[ 'password' ] ) )
+            return $_REQUEST[ 'password' ];
+        else
+            return false;
+    }
+    
+    /*
+     * restituisce i dati inseriti nella form per la restituzione e li inserisce in un array
+     */
+    public function getDati() {
+        $dati = array( 'nome', 'cognome', 'email', 'password', 'conf_password', 'sesso', 'citta', 'data_nas' );
+        foreach ( $dati as $data ) {
+            if( isset( $_REQUEST[ $data ] ) )
+                $dati[ $data ] = $_REQUEST[ $data ];
+        }
+        return $dati;    
+    }
+    
+    /*
+     * imposta l'errore per visualizzare l'errore
+     */
+    public function stampaErrore( $errore ){
+        //assegna $errore al template relativo
+    } 
+}
+
+?>
