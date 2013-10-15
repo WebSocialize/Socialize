8b291bd8fc258d140ca14284b765757b4440eb62
alessscop <alessandroscopigno@gmail.com>
2013-10-15 15:12:41 +0200

    aggiunta creaUtente()

--- Control/CRegistrazione.php
+++ Control/CRegistrazione.php
@@ -19,7 +19,33 @@ class CRegistrazione {
          * a VRegistrazione.
          * Prima bisogna capire come usare Singleton per fare una sola istanza etc
          */
+        $view = USingleton::getInstance( 'VRegistrazione' );
+        $dat_reg = $view->getDati();       //dati immessi dall'utente
+        $result = load( $dat_reg[ 'email' ] );
+        $errore;
+        if( !$result ){
+            //email non usata ancora
+            if( $dat_reg[ 'password' ] == $dat_reg[ 'conf_password' ]){
+                $utente = new Eutente($dat_reg['nome'], $dat_reg['cognome'], $dat_reg['data_nas'], $dat_reg['password'], $dat_reg['sesso'], $dat_reg['email'], $dat_reg['citta']);
+                $utente->generaCodiceAttivazione();
+                $Fuser = new FUtente();
+                $Fuser->store($utente);
+                $this->EmailAttiv();
+            }
+            else{
+                //le password non coincidono
+                $errore = 'Le password inserite non coincidono!';
+            }
+        }    
+        else{
+            $errore = 'usare un\' altra mail: quella inserita è già stata immessa!';
+        }
         
+        if( !$errore ){
+            /*
+             * DOBBIAMO VEDERE LA PARTE RELATIVA AI TEMPLATE!!!
+             */
+        }
     }
     
     /*
